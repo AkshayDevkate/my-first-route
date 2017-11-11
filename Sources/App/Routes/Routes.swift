5e312125a1bb1339c5d1786beb656ab5a2065d3b
martinlasek <martinlasek@hotmail.de>
2017-11-11 14:35:27 +0100

    updated for swift 4

---
+++ Sources/App/Routes/Routes.swift
@@ -0,0 +1,17 @@
+import Vapor
+
+extension Droplet {
+  func setupRoutes() throws {
+  
+    get("name") { req in
+      return "Ethan Hunt"
+    }
+    
+    get("age") { req in
+      return "\(23)"
+    }
+    get("json") { req in
+      return try JSON(node: ["name": "Martin J. Lasek", "age": 26])
+    }
+  }
+}
